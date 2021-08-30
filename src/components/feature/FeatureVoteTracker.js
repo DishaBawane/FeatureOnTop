88b03ca02e96e796f328f9b8f1bc9f13b00a376a
amol bawane <dishakhode@gmail.com>
2021-08-30 10:47:54 +0530

    second commit

--- src/components/feature/FeatureVoteTracker.js
+++ src/components/feature/FeatureVoteTracker.js
@@ -4,23 +4,26 @@ import classes from "./FeatureVoteTracker.module.css";
 
 const FeatureVoteTracker = (props) => {
   return (
-    <Card>
-      <div className={classes.featurelist}>
-        {/* <div className={classes.votetracker}>
+    <div>
+      {props.features.map((feature) => (
+        <Card>
+          <div className={classes.featurelist}>
+            {/* <div className={classes.votetracker}>
           <p className={classes.addvote}>👍</p>
           <p className={classes.totalvotes}> 10</p>
           <p className={classes.addvote}>👎</p>
         </div> */}
-        <div>
-          {props.features.map((feature) => (
-            <p className={classes.feature}>{feature.txt}</p>
-          ))}
-        </div>
-      </div>
-      {/* <div>
+            <div>
+              {/* <p className={classes.feature}>{feature.txt}</p> */}
+              <input className={classes.feature}>{feature.txt}</input>
+            </div>
+          </div>
+          {/* <div>
         <p className={classes.date}>14 Aug 2021</p>
       </div> */}
-    </Card>
+        </Card>
+      ))}
+    </div>
   );
 };
 export default FeatureVoteTracker;
